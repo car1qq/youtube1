273f1f1f41c1722c9a01b3459ca2ffff061f1291
car1qq <94748125+car1qq@users.noreply.github.com>
2022-07-16 18:45:09 +0800

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,13 +1,13 @@
 import axios from "axios";
 
-const key = "AIzaSyCv6CTmpIAUF08FFBqRaatq-SbZHFB-olM";
+const key = "AIzaSyBLo9CQLAZyT9gUbf2Y-S0KXhW0PFGQpXo";
 
 export default axios.create({
     baseURL: "https://www.googleapis.com/youtube/v3",
     params: {
         part: 'snippet',
         type: 'video',
-        maxResults: 12,
+        maxResults: 100,
         key,
     },
 });
